db1c9c2faab5a6f193b284dc17fdc58520f13557
Drengifo <Danielrengifotorres@gmail.com>
2026-04-07 06:50:31 -0400

    Update app.py

--- sqc-main/webapp/app.py
+++ sqc-main/webapp/app.py
@@ -18,8 +18,8 @@ from PIL import Image, ImageDraw, ImageFont
 
 app = Flask(__name__)
 
-# === Rutas (según tu proyecto) ===
-BASE_DIR = "/home/qwid94/sqc-main"
+# === Rutas ===
+BASE_DIR = "/home/sqc/sqc-main"
 PHOTO_FOLDER = f"{BASE_DIR}/fotos"
 
 # === Estado global ===
